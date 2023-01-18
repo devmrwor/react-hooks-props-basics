9878cbe88fe15486842fd356b1ce8a79314b754c
devmrwor <michaelryanrm18@gmail.com>
2023-01-18 13:47:46 -0800

    add fake test

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+// The code below ensures that students who are using CodeGrade will get credit
+// for the code-along in Canvas; you can disregard it.
+
+test("Test passing", () => {
+  return new Promise((resolve, reject) => {
+    resolve(true);
+  });
+});
